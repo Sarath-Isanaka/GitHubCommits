66199a5d8cf2b5bfc8ec1672e7243f97aae37acb
sarath-isanaka <sarathisanaka@gmail.com>
2022-10-10 03:25:08 +0530

    Fetched Data from GIT using Octokit

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,25 +1,43 @@
 import React, { useEffect } from "react";
 import { Octokit } from "@octokit/core";
 import "./home.css";
+import { useState } from "react";
+import moment from 'moment'
 
 function Home() {
-  const octokit = new Octokit({ auth: `` });
-  
+  const [commitData, setCommitData] = useState([]);
+  const octokit = new Octokit({
+    auth: `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`,
+  });
+
   useEffect(() => {
     fetchGitData();
   }, []);
 
-  async function fetchGitData(){
-    const response = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-      owner: 'Sarath-Isanaka',
-      repo: 'Covid-Tracker'
-    })
-    console.log(response)
+  async function fetchGitData() {
+    const response = await octokit.request(
+      "GET /repos/{owner}/{repo}/commits",
+      {
+        owner: "Sarath-Isanaka",
+        repo: "Covid-Tracker",
+      }
+    );
+    console.log(response.data);
+    console.log(response.data[0].commit.message);
+    console.log(response.data[0].commit.committer.date)
+    var a = await moment(response.data[0].commit.committer.date).format('LLL')
+    console.log(a)
+    await setCommitData([...response.data]);
   }
   return (
     <React.Fragment>
       <div className="center">
-
+        {commitData.map((commit) => (
+          <>
+          <h2>{commit.commit.message}</h2>
+          <p>{moment(commit.commit.committer.date).format('LLL')} <b>by {commit.commit.committer.name}</b> </p>
+          </>
+        ))}
       </div>
     </React.Fragment>
   );
