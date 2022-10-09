12ab59a78c701254f37eb220d86f2550a95bcb98
sarath-isanaka <sarathisanaka@gmail.com>
2022-10-10 05:22:16 +0530

    Final Changes

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { Octokit } from "@octokit/core";
 import "./home.css";
 import { useState } from "react";
@@ -6,45 +6,77 @@ import moment from "moment";
 
 function Home() {
   const [commitData, setCommitData] = useState([]);
-  const octokit = new Octokit({
-    auth: ``,
-  });
+  const [count, setCount] = useState(30);
+  const [token, setToken] = useState("");
+  const tokenRef = useRef();
 
   useEffect(() => {
-    fetchGitData();
-  }, []);
+    fetchPersonalTokenFromStorage();
+    token && fetchGitData();
+  }, [token]);
+
+  useEffect(() => {
+    if (count > 0) {
+      setTimeout(() => setCount(count - 1), 1000);
+    } else {
+      fetchGitData();
+      setCount(30)
+    }
+  }, [count]);
+
+  async function fetchPersonalTokenFromStorage() {
+    const localToken = await localStorage.getItem("personal token");
+    await setToken(localToken);
+  }
+
+  function tokenSubmitHandler(e) {
+    localStorage.setItem("personal token", tokenRef.current.value);
+    setToken(tokenRef.current.value);
+  }
 
   async function fetchGitData() {
+    const octokit = new Octokit({
+      auth: `${token}`,
+    });
+
     const response = await octokit.request(
       "GET /repos/{owner}/{repo}/commits",
       {
         owner: "Sarath-Isanaka",
-        repo: "Covid-Tracker",
+        repo: "GitHubCommits",
       }
     );
-    console.log(response.data);
-    console.log(response.data[0].commit.message);
-    console.log(response.data[0].commit.committer.date);
-    var a = await moment(response.data[0].commit.committer.date).format("LLL");
-    console.log(a);
     await setCommitData([...response.data]);
   }
+
   return (
     <React.Fragment>
-      <div className="center">
-        {commitData.map((commit) => (
-          <>
-            <div className="container">
-              <h3>{commit.commit.message}</h3>
-              <p>
-                {moment(commit.commit.committer.date).format("LLL")}{" "}
-                <b>by {commit.commit.committer.name}</b>{" "}
-              </p>
-            </div>
-            <div className="gradient"></div>
-          </>
-        ))}
-      </div>
+      {token ? (
+        <div className="center">
+          {commitData.map((commit, index) => (
+            <React.Fragment key={index}>
+              <div className="container">
+                <h3>{commit.commit.message}</h3>
+                <p>
+                  {moment(commit.commit.committer.date).format("LLL")}{" "}
+                  <b>by {commit.commit.committer.name}</b>{" "}
+                </p>
+              </div>
+              <div className="gradient"></div>
+            </React.Fragment>
+          ))}
+          <div>
+            <button onClick={fetchGitData}>Refresh Commits</button>
+            <span className="counter">{count}</span>
+          </div>
+        </div>
+      ) : (
+        <div className="inputToken">
+          <h2 className="tokenHeader">Personal Token Key</h2>
+          <input ref={tokenRef}></input>
+          <button onClick={tokenSubmitHandler}>Submit</button>
+        </div>
+      )}
     </React.Fragment>
   );
 }
